dfdaab91099587d96111a9b0a5ee3b5d6ae559b2
ATPStorages <pmt.mailservice@gmail.com>
2024-06-17 00:34:03 -0500

    Structure changes, alire

--- Source/Main/UEFI/uefi.adb
+++ Source/Main/UEFI/uefi.adb
@@ -1,9 +1,12 @@
+with ADA.CHARACTERS.LATIN_1;
+with Ada.Characters.Conversions;
+
 package body UEFI is
    function EFIMain(ImageHandle : EFI_HANDLE; SystemTable : EFI_SYSTEM_TABLE) return EFI_STATUS is
       unused: Unsigned_32;
    begin
       InitializeLib(ImageHandle, SystemTable);
-      unused := Print("\0");
+      unused := Print(Ada.Characters.Conversions.To_Wide_String("Hi" & Ada.Characters.Latin_1.NUL));
       --unused := SystemTable.ConOut.OutputString(SystemTable.ConOut, "Test");
       return 0;
    end;
